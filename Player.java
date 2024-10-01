17dfa51adc43feec1093c45d523fcc1396a173d4
Phreathia <89539784+Phreathia@users.noreply.github.com>
2024-10-01 12:57:32 +0000

    monster and player

---
+++ Player.java
@@ -0,0 +1,5 @@
+public class Player {
+    // static variables
+    public static int health = 100;
+    
+}
---
+++ monsters/Monster.java
@@ -0,0 +1,46 @@
+package monsters;
+
+public abstract class Monster {
+    // encapsulated variables
+    private int health;
+    private int levelModifier;
+    private int minDamage;
+    private int maxDamage;
+    private String name;
+
+    // blank constructor
+    public Monster() {
+        health = 100;
+        levelModifier = 1;
+        minDamage = 1;
+        maxDamage = 10;
+        name = "Monster";
+    }
+
+    // constructor with params
+    public Monster(int h, int l, int min, int max, String n) {
+        health = h;
+        this.levelModifier = l;
+        this.minDamage = min;
+        this.maxDamage = max;
+        this.name = n;
+    }
+
+    // health modifier
+    public void setHealth(int health){
+        this.health = health;
+    }
+
+    // accessors
+    public int getHealth() {
+        return health;
+    }
+
+    public int getLevelModifier() {
+        return levelModifier;
+    }
+
+    public int getDamage() {
+        return (int) (Math.random() * (maxDamage - minDamage) + minDamage);
+    }
+}
